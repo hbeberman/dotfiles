b4062f264512f1148b8da06458bb99e799787c91
Henry Beberman <henry.beberman@microsoft.com>
2025-10-12 17:12:06 -0700

    fish+alacritty: switch to fish shell, autocolor on host

---
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,45 @@
+function fish_prompt
+    # Colors
+    set -l grey    (set_color -o 949494)
+    set -l blue    (set_color -o 00afff)
+    set -l jupiter (set_color -o D88C3E)
+    set -l neptune (set_color -o 00afff)
+    set -l white   (set_color -o ffffff)
+    set -l reset   (set_color normal)
+
+    # Cache hostname and delim color once
+    if not set -q __prompt_host
+        set -g __prompt_host (
+            command hostnamectl hostname |
+            string trim |
+            string lower |
+            string split -f1 '.'
+        )
+
+        switch $__prompt_host
+            case neptune
+                set -g __delim_col $neptune
+            case jupiter
+                set -g __delim_col $jupiter
+            case '*'
+                set -g __delim_col $white
+        end
+    end
+
+    set -l host $__prompt_host
+    set -l cwd (prompt_pwd)
+
+    # Optional prefix for SSH or virtualized sessions
+    set -l prompt_host ""
+    if set -q SSH_TTY
+        or begin
+            command -sq systemd-detect-virt
+            and systemd-detect-virt -q
+        end
+        set prompt_host "$__delim_col$host$reset "
+    end
+
+    # Print prompt
+    echo -n "$prompt_host$grey$cwd $__delim_col\$ $reset"
+end
+
