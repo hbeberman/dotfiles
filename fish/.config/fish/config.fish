730fd7998cfe8d0e761134e588894bdd98d63b5c
Henry Beberman <henry.beberman@microsoft.com>
2025-12-04 00:20:53 -0800

    fish: add vim keybindings, including ctrl j/k history scrolling

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # Move down in history with Ctrl+j
+    bind \cj down-or-search
+
+    # Move up in history with Ctrl+k
+    bind \ck up-or-search
+
+    fish_vi_key_bindings
+
+    function fish_mode_prompt
+      if test "$fish_bind_mode" = "default"
+            set_color -o FF7F50
+            echo -n "# $reset"
+      end
+    end
 end
